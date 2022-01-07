951c86b251952549e1279b0c0aa9b8a303b80520
Antonio Enríquez <antonio.enralv@gmail.com>
2022-01-07 17:28:12 -0600

    201

---
+++ test.js
@@ -0,0 +1,11 @@
+function escribirIF26_1_452(){     
+    var if1 = "if(array1[i]==26 && array2[j]==";      
+    var if2 = "){"  
+    var if2_1 = "distancia = 0;";  
+    var if3 = "console.log(distancia);"  
+    var if4 = "}"  
+  for(var i=1; i <= 452; i++){   
+      document.write(if1 + i + if2 + ''+ if2_1 +'' + if3 + '' + if4 + '' );  
+  }  
+}
+escribirIF26_1_452()
